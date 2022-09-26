80de1fd0437012b3cefa28f009a20ff1dbb24e04
Ahzam Akhtar <ahzam8186@gmail.com>
2022-09-26 20:26:16 +0530

    Update User Data

--- app/src/main/java/com/team/hackathon/eventregisteration/ui/RegisterForEventFragment.kt
+++ app/src/main/java/com/team/hackathon/eventregisteration/ui/RegisterForEventFragment.kt
@@ -1,7 +1,6 @@
 package com.team.hackathon.eventregisteration.ui
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,12 +9,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import com.razorpay.Checkout
-import com.team.hackathon.R
 import com.team.hackathon.databinding.FragmentRegisterForEventBinding
 import com.team.hackathon.eventregisteration.util.EventRegistrationViewModel
-import org.json.JSONException
-import org.json.JSONObject
 
 
 class RegisterForEventFragment : Fragment() {
@@ -36,9 +31,7 @@ class RegisterForEventFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
 
-        val secondTeamMemberEmail = binding.email2.text
-        val thirdTeamMemberEmail = binding.email3.text
-        val fourthTeamMemberEmail = binding.email4.text
+
 
         binding.teamMemeberOne2.visibility = View.GONE
         binding.teamMemeberOne3.visibility = View.GONE
@@ -73,48 +66,11 @@ class RegisterForEventFragment : Fragment() {
             binding.teamMemeberOne4.visibility = View.GONE
             binding.inviteButton3.visibility = View.VISIBLE
         }
-        binding.bottomButton.setOnClickListener{
-            val amt = "10"
-            val amount = Math.round(amt.toFloat() * 100)
-            val checkout = Checkout()
-
-            checkout.setKeyID("rzp_test_lDBxxnlPFiPUGx")
-
-            checkout.setImage(R.drawable.add_icon)
-            val obj = JSONObject()
-            try {
-                obj.put("name", "Tech-A-Thon")
-
-                // put description
-                obj.put("description", "Test payment")
-
-                // to set theme color
-                obj.put("theme.color", "")
-
-                // put the currency
-                obj.put("currency", "INR")
-
-                // put amount
-                obj.put("amount", amount)
-
-                // put mobile number
-                obj.put("prefill.contact", "8082731286")
-
-                // put email
-                obj.put("prefill.email", "tahir19.fm@gmail.com")
-
-                checkout.open(requireActivity(),obj)
-            }catch (e: JSONException){
-                e.printStackTrace()
-            }
-        }
 
 
         binding.saveButton.setOnClickListener{
-            val firstTeamMemberEmail = binding.email.text
             val firstTeamMemberName = binding.name.text
-            sendEmail(firstTeamMemberEmail.toString() , "Subject" + "$firstTeamMemberName" , emailMessage )
-
+            sendEmail()
             // yoo
 
         }
@@ -145,13 +101,14 @@ class RegisterForEventFragment : Fragment() {
 
     }
 
-    private fun sendEmail(email:String , subject:String, message:String){
-        val intent = Intent(Intent.ACTION_SEND)
-        intent.data = Uri.parse("mailto:$email")
-        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
-        intent.putExtra(Intent.EXTRA_TEXT, message)
-        intent.type = "message/rfc822"
-        startActivity(Intent.createChooser(intent, "Select email"))
+    private fun sendEmail(){
+        val sendIntent = Intent()
+        sendIntent.action = Intent.ACTION_SEND
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
+        sendIntent.type = "text/plain"
+        sendIntent.setPackage("com.whatsapp")
+        startActivity(Intent.createChooser(sendIntent, ""))
+        startActivity(sendIntent)
     }
 
 }
