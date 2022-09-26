0764f165ca97c85312fab5c2c79bdc6bb47bf6be
Ahzam Akhtar <ahzam8186@gmail.com>
2022-09-27 04:36:20 +0530

    Update User Data

--- app/src/main/java/com/team/hackathon/UserProfile/ui/FragmentUserProfile.kt
+++ app/src/main/java/com/team/hackathon/UserProfile/ui/FragmentUserProfile.kt
@@ -7,7 +7,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.google.firebase.auth.FirebaseAuth
@@ -82,7 +81,6 @@ class FragmentUserProfile : Fragment()  {
                         )
                     )
                     setData()
-                    Toast.makeText(requireActivity(),name,Toast.LENGTH_SHORT).show()
                     showALlContentHideProgresssBar()
                 }else{
                     Log.d("Data", "No such document")
